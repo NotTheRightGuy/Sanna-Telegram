d4325e32c120e01eb3c13dd081ecc52c1830db69
NotTheRightGuy <janmejaychatterjee@gmail.com>
2021-10-24 20:41:09 +0530

    Added New feat.

--- helper_function.py
+++ helper_function.py
@@ -2,7 +2,10 @@ import yfinance as yf
 
 def stock_history(stock_name):
     stock_ticker = yf.Ticker(stock_name)
-    stock_info_dic = stock_ticker.info
     hist = stock_ticker.history(period='max')
-    #return stock_info_dic
     return hist
+
+def stock_info(stock_name):
+    stock_ticker = yf.ticker(stock_name)
+    stock_info_dic = stock_ticker.info
+    return stock_info_dic
